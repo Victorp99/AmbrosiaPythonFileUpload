081ccb4772a97b0e1c9d9ba38e129745f7397bb4
Shaun Stasik <stasiks631@ascg.strose.edu>
2016-04-08 21:18:33 -0400

    Integrated all files to establish working baseline code.

--- upload.php
+++ upload.php
@@ -1,4 +1,7 @@
 <?php
+  set_include_path(get_include_path() . PATH_SEPARATOR . 'phpseclib');
+  include('Net/SSH2.php');
+  
   // Report all errors
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
@@ -6,19 +9,19 @@
   #check to see if anything was uploaded
   #php uses the temporary name of the file uploaded and not the actual name of the file. We need to use the temporary file
   if(!file_exists($_FILES['fileToUpload']['tmp_name']) || !is_uploaded_file($_FILES['fileToUpload']['tmp_name'])) {
-    echo 'No upload';
+	exit('No upload');
   }
   #check to see if file uploaded is a python file
-  if($_FILES['fileToUpload']['type'] != "text/plain"){
-    echo "You did not upload a python file.";
-  }
+  /*if($_FILES['fileToUpload']['type'] != "text/plain"){
+	exit("You did not upload a python file.");
+  }*/
   #check to see if ambrosia is being uploaded by checking for: from ambrosia import *
   #echo file_get_contents($_FILES['fileToUpload']['tmp_name']);
   $fileContents = file_get_contents($_FILES['fileToUpload']['tmp_name']);
   function multiexplode ($delimiters,$string) {
-    $ready = str_replace($delimiters, $delimiters[0], $string);
-    $launch = explode($delimiters[0], $ready);
-    return  $launch;
+	$ready = str_replace($delimiters, $delimiters[0], $string);
+	$launch = explode($delimiters[0], $ready);
+	return  $launch;
   }
   $words = multiexplode(array(" ","\n","\r","\t"),$fileContents); #stores each word into an array
   $x = 0;
@@ -30,41 +33,93 @@
   $sProcessCheck = false; #catch all instances of subprocess
   $shCheck = false;
   while($y < count($words) -1 && $noAmbrosia == false){
-    #ambrosia import statement was found
-    if($words[$y] == "from" && $words[$y + 1] == "ambrosia" && $words[$y + 2] == "import" && $words[$y + 3] == "*"){
-      $noAmbrosia = true;
-    }
-    #from subprocess import call was found. Used to make shell commands
-    if($words[$y] == "from" && $words[$x + 1] == "subprocess" && $words[$x + 2] == "import" && $words[$x + 3] == "call"){
-      $subProcessCheck = true;
-    }
-    #used for quick scripts
-    if($words[$y] == "os.system" || $words[$y] == "os"){
-      $osSystemCheck = true;
-    }
-    if($words[$y] == "subprocess"){
-      $sProcessCheck = true;
-    }
-    if($words[$y] == "from" && $words[$y] == "sh"){
-      $shCheck = true;
-    }
-    $y++;
+	#ambrosia import statement was found
+	if($words[$y] == "from" && $words[$y + 1] == "ambrosia" && $words[$y + 2] == "import" && $words[$y + 3] == "*"){
+	  $noAmbrosia = true;
+	}
+	#from subprocess import call was found. Used to make shell commands
+	if($words[$y] == "from" && $words[$x + 1] == "subprocess" && $words[$x + 2] == "import" && $words[$x + 3] == "call"){
+	  $subProcessCheck = true;
+	}
+	#used for quick scripts
+	if($words[$y] == "os.system" || $words[$y] == "os"){
+	  $osSystemCheck = true;
+	}
+	if($words[$y] == "subprocess"){
+	  $sProcessCheck = true;
+	}
+	if($words[$y] == "from" && $words[$y] == "sh"){
+	  $shCheck = true;
+	}
+	$y++;
   }
   #ambrosia import statement was not in the file uploaded
   if($noAmbrosia == false){
-    echo "This file does not contain an Ambrosia import statement.";
+	exit("This file does not contain an Ambrosia import statement.");
   }
   if($subProcessCheck == true){
-    echo "Error. Dangerous statement was found. Found: subprocess";
+	exit("Error. Dangerous statement was found. Found: subprocess");
   }
   if($osSystemCheck == true){
-    echo "Error. Dangerous statement was found. Found: os.system";
+	exit("Error. Dangerous statement was found. Found: os.system");
   }
   if($sProcessCheck == true){
-    echo "Error. Dangerous statement was found. Found: subprocess";
+	exit("Error. Dangerous statement was found. Found: subprocess");
   }
   if($shCheck == true){
-    echo "Error. Dangerous statement was found. Found: from sh";
+	exit("Error. Dangerous statement was found. Found: from sh");
   }
 
+  //if validation of file was successful, run the python file and return the output
+  echo '<head><title>Ambrosia</title></head><body>';
+  echo 'You have requested to run the file ' . $_FILES['fileToUpload']['name'] . "<br>";
+  
+  #changer permission of the uploaded file so that it can be moved after ssh
+  system('chmod 777 ' . $_FILES['fileToUpload']['tmp_name']);
+  
+  #log in via ssh with ascg username and password
+  $ssh = new Net_SSH2('ascg.strose.edu');
+  if (!$ssh->login($_POST["username"], $_POST["password"])) {
+	exit('Login Failed');
+  }
+
+  #move the uploaded python to the user's home area
+  $fmove = $ssh->exec('cp ' . $_FILES['fileToUpload']['tmp_name'] . ' ./' . $_FILES['fileToUpload']['name']);
+
+  #get the shell
+  $checksh = $ssh-->exec('echo $0');
+
+  #if bash shell, set python path and run file; otherwise just run file.
+  if ($checksh == '-bash'){
+	$output = $ssh->exec('export PYTHONPATH=${PYTHONPATH}:/home/ascg/ambrosia/python/; python3 ' . $_FILES['fileToUpload']['name']);
+  }
+  else{
+	  $output = $ssh->exec('python3 ' . $_FILES['fileToUpload']['name']);
+  }
+  echo 'Output: <br> ' . $output . "<br>";
+
+  #process the output
+  $outnametemp = explode(":", $output);
+  $outnametemp2 = explode("[", $outnametemp[0]);
+  $outnametemp3 = explode(".", $outnametemp2[1]);
+  $outputfilename = $outnametemp3[0];
+  #echo $outputfilename . "<br>";
+  $tmpimage = '/tmp/' . $outputfilename . '.png';
+  $log = '/tmp/' . $outputfilename . '.log';
+  #echo $tmpimage . "<br>";
+  #echo $log . "<br>";
+  
+  #move the image file so that it is accessible
+  echo $ssh->exec('mv ' . $tmpimage . ' /usr/home/www/ambrosia/images/');
+  echo '<br>';
+  #remove the python file that was run
+  echo $ssh->exec('rm ' . $_FILES['fileToUpload']['name']);
+  echo '<br>';
+  
+  #display the image file
+  $image = './images/' . $outputfilename . '.png';
+  
+  echo '
+    <img src=' . $image . '>
+    </body>'
  ?>
